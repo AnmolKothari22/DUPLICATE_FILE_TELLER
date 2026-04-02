8a9de096f0634ab06cf13a1479f2d0739f672177
Anmol Kothari <anmolkothari22@gmail.com>
2026-04-02 18:48:10 +0530

    commented on duplifile script

--- two_file_compare/duplifile.sh
+++ two_file_compare/duplifile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#handling no input given case....direct exit
 if [ "$#" -eq 0 ]; then
 	echo "please give atleast one file path"
 	exit
@@ -8,55 +9,54 @@ fi
 
 declare -A maps
 
+# to add a file's path and hash 
 add() {
 	local key=$1
 	local value=$2
 
-		     
+	#if key doesn't exist in map 
 	if [[ -z "${maps[$key]}" ]]; then
-	local arr="arr_$key"
+	local arr="arr_$key" #creating array for key
 	maps[$key]=$arr
 	declare -g -a "$arr=()"
 	fi
 		    
 		                                         
 	declare -n ref=${maps[$key]}
-	ref+=("$value")
+	ref+=("$value") #adding values
 }
 
-
+#iterating over paths
 for arg in $@ ; do
-	if [ -d $arg ]; then
-		#echo "directory given"
-		for arg2 in $arg/* ; do
-			#echo $arg2
+	if [ -d $arg ]; then # if the path points to a directory
+		#NOTE: its non recursive!!
+		for arg2 in $arg/* ; do #for files in directory 
 			if [ -f $arg2 ]; then
-				TEM=$(  sha256sum $arg2 | awk '{print $1}' )
-				#echo $TEM
+				TEM=$(  sha256sum $arg2 | awk '{print $1}' ) # generating hash
 				add $TEM $arg2
 			fi
 		done	
 	else
-		
 		TEM=$(  sha256sum $arg | awk '{print $1}' )
 		#echo $TEM
 		add $TEM $arg
 	fi
 done
 
+#iterating over map
 for key in "${!maps[@]}"; do
-    declare -n ref="${maps[$key]}"
+    declare -n ref="${maps[$key]}" #referance to array of key
 
     echo -n "same files on following path ->"
 
-    for i in "${!ref[@]}"; do
+    for i in "${!ref[@]}"; do #iterating over map's values
         val="${ref[$i]}"
-
-        if [[ "$val" == *:* ]]; then
-            ref[$i]="${val#*:}"   # remove everything up to first colon
+		#cleanup begin
+        if [[ "$val" == *:* ]]; then #only for elements with : , 
+            ref[$i]="${val#*:}"   # remove everything up to first colon cause python script makes files like :DUPLICATE_FILE_TELLER/two_file_compare:actual path: ...so actual path is extracted from here 
         fi
-
-        echo  -n " ${ref[$i]} , "
+		#cleanup ends
+        echo  -n " ${ref[$i]} , " 
     done
 	printf "\n"
 done
